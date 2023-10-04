92730802edb3384bbb48ea087e842810d0a6baff
TheKeeroll <57570053+TheKeeroll@users.noreply.github.com>
2023-10-04 12:03:44 +0300

    update

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/sh
-
+cd "$(dirname "$0")"
 if [ $# -eq 0 ]
   then
     echo "No container name supplied"
     echo "Usage ./deploy.sh <CONTAINER_NAME> <SSH_BIND_PORT>"
+    exit
 fi
-
+SSH_BIND=""
 if [ -z "$2" ]
   then
-    SSH_BIND = "-p"
+    SSH_BIND="-p"
 else
-  SSH_BIND = "-p $2"
+  SSH_BIND="-p $2"
 fi
 
-cd ..
-sudo docker build -t "$CONTAINER_NAME_image" .
-sudo docker run -d --name $CONTAINER_NAME $SSH_BIND
+sudo docker build -t "${1}_image" ..
+sudo docker run "${1}_image"  -d --name "$1" "$SSH_BIND"
--- docker/entry.sh
+++ docker/entry.sh
@@ -4,6 +4,5 @@ cd /app
 npm i --production
 npm run build
 rm -rf app
-rm .eslintrc.json .gitignore next-dev.d.ts README.md tailwind.consif.ts tsconfig.json
 npm start
 tail -f /dev/null
