2b5883839d8e0e832155853666190cdcf4b5460a
TheKeeroll <57570053+TheKeeroll@users.noreply.github.com>
2023-10-04 11:49:03 +0300

    update

---
+++ docker/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin sh
+
+if [ $# -eq 0 ]
+  then
+    echo "No container name supplied"
+    echo "Usage ./deploy.sh <CONTAINER_NAME> <SSH_BIND_PORT>"
+fi
+
+if [ -z "$2" ]
+  then
+    SSH_BIND = "-p"
+else
+  SSH_BIND = "-p $2"
+fi
+
+cd ..
+sudo docker build -t "$CONTAINER_NAME_image" .
+sudo docker run -d --name $CONTAINER_NAME $SSH_BIND 
